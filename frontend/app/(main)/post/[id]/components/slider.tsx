9a9c54091e5d9ae95285b391ac16c7832c19052d
Jason Cheng <jasoncheng224@gmail.com>
2024-03-09 18:12:02 -0800

    slider

---
+++ frontend/app/(main)/post/[id]/components/slider.tsx
@@ -0,0 +1,32 @@
+import { useRef } from "react";
+
+export default ({ frac, setFrac }: { frac: number, setFrac: (_: number) => void }) => {
+    const sliderRef = useRef<HTMLDivElement>(null);
+
+    const startDrag = (e0: React.MouseEvent<HTMLDivElement>) => {
+        if (sliderRef.current === null)
+            return;
+
+        const bbox = sliderRef.current.getBoundingClientRect();
+
+        const calcFrac = (x: number) => Math.min(1, Math.max(0, (x - bbox.x) / bbox.width));
+
+        setFrac(calcFrac(e0.clientX));
+
+        const handleDrag = (e: MouseEvent) => setFrac(calcFrac(e.x));
+
+        const endDrag = (e: MouseEvent) => {
+            setFrac(calcFrac(e.x));
+            document.removeEventListener("mousemove", handleDrag);
+        };
+
+        document.addEventListener("mousemove", handleDrag);
+        document.addEventListener("mouseup", endDrag, { once: true });
+    };
+
+    return (
+        <div className="slider" onMouseDown={startDrag} ref={sliderRef}>
+            <div className="fill" style={{ width: `${frac * 100}%` }}></div>
+        </div>
+    )
+};
--- frontend/app/(main)/post/[id]/page.tsx
+++ frontend/app/(main)/post/[id]/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+
+import { useState } from "react";
+import Slider from "./components/slider";
+import "./post-details.css";
+
 export default ({ params: { id } }: { params: { id: string } }) => {
+    const [frac, setFrac] = useState(0.5);
+
     return (
-        <p>Hi { id }</p>
+        <main>
+            <img src="/tango.jpg" />
+            <p>
+                Caption goes here
+            </p>
+            <Slider frac={frac} setFrac={setFrac} />
+        </main>
     );
 };
