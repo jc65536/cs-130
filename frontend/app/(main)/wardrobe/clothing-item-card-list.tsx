5d8fbe1cdd1a4c913ec8920f79a98a4089ae6d86
Darren <darrenzhang22@gmail.com>
2024-03-12 14:25:31 -0700

    fix fetch error

--- frontend/app/(main)/wardrobe/clothing-item-card-list.tsx
+++ frontend/app/(main)/wardrobe/clothing-item-card-list.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
+"use client";
+
+import React, { useState, useEffect } from 'react';
 import ClothingItemCard, { Clothing } from './clothing-item-card';
+import { backend_url } from '../../settings';
 import '@/app/card.css';
 
 // Dummy data for demonstration
-var clothings: Clothing[] = [];
-for (let id = 1; id <= 20; id++) {
-    clothings.push({ id, name: `Caption ${id}`, rating: 5 });
-}
+// var clothings: Clothing[] = [];
+// for (let id = 1; id <= 20; id++) {
+//     clothings.push({ id, name: `Caption ${id}`, rating: 5 });
+// }
 
 
 export default function ClothingItemCardList() {
+  const [clothings, setClothing] = useState<Clothing[]>([]);
+
+  useEffect(() => {
+    fetch(backend_url(`/wardrobe/clothes`), { credentials: "include" })
+      .then(res => res.json())
+      .then(setClothing);
+  }, []);
+
   return (
     <div className="clothing-list">
       {clothings.map((clothing, i) => (
-        // Assuming PostItemCard accepts props for id and caption
-        <ClothingItemCard key={i} id={clothing.id} name={clothing.name} rating={clothing.rating} />
+        <ClothingItemCard key={i} {...clothing} />
       ))}
     </div>
   );
--- frontend/app/(main)/wardrobe/clothing-item-card.tsx
+++ frontend/app/(main)/wardrobe/clothing-item-card.tsx
@@ -1,40 +1,71 @@
 "use client";
 
-import React, { MouseEvent } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { backend_url } from '@/app/settings';
+import { Post } from '@/app/components/post-item-card';
 
 export type Clothing = {
     id: number,
     name: string,
-    rating: number
+    rating: number,
+    reusedCount: number,
+    cost: number,
+    onSale: Boolean,
+    posts: string[]
 }
 
-export default function ClothingItemCard({ id, name, rating }: Clothing) {
+export default function ClothingItemCard({ id, name, rating, reusedCount, cost, onSale, posts }: Clothing) {
+    const [postState, setPosts] = useState<Post[]>([]);
     // Assume the detail page route is '/clothing/[id]', where [id] is a dynamic segment
     const detailPagePath = `/clothing/${id}`;
 
-    const toggleDetails = (e: MouseEvent) => {
-        if (! (e.currentTarget instanceof HTMLElement))
-            return;
-        if (e.currentTarget.children) {
-            
-        }
-    };
-            
+    // const toggleDetails = (e: MouseEvent) => {
+    //     if (!(e.currentTarget instanceof HTMLElement))
+    //         return;
+    //     if (e.currentTarget.children) {
+
+    //     }
+    // };
+    useEffect(() => {
+        fetchPostFilenames();
+    }, []);
+
+    const fetchPostFilenames = async () => {
+        const postObjs = await Promise.all(posts.map(async postId => {
+            const res = await fetch(backend_url(`/posts/${postId}`), { credentials: "include" });
+            const json = await res.json();
+            return json;
+        }));
+        setPosts(postObjs);
+    }
+
 
     return (
-        <div className="clothing-card" onClick={toggleDetails}>
-            <div className='clothing-image'>
+        <div className="clothing-card card" >
+            <div className='clothing-image-grid'>
                 <Link href={detailPagePath}>
-                    <Image
+                    {
+                        postState.map(post => 
+                            <img 
+                                src={backend_url(`/posts/image/${post.imageFilename}`)}
+                                alt={post.caption}
+                            />    
+                        )
+                    }
+                    {/* <Image
                         // src={`https://picsum.photos/seed/${id}/120/160`}
                         src={`https://picsum.photos/id/${id}/120/160`}
                         alt={name}
                         width={140}
                         height={200}
-                    />
+                    /> */}
                 </Link>
+            </div>
+            <div className='clothing-text-content'>
+                <span>{name}</span>
+                <span>{"Number of outfits used in: " + reusedCount}</span>
             </div>
         </div>
     );
