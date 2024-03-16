bf94e17e6616b7774a4085b8230510c8dc9392e6
Darren <darrenzhang22@gmail.com>
2024-03-15 19:06:20 -0700

    Add mocking of getClient()

---
+++ backend/lib/db-lib/__mocks__/db-client.ts
@@ -0,0 +1,29 @@
+import { ObjectId } from "mongodb";
+
+export const getClient = jest.fn(() => {
+  return {
+      findDbItem: jest.fn((_, id) => {
+          console.log("mock findDbItem called: "+id)
+          const userId = new ObjectId("123456789012345678901234");
+          const wardrobeId = new ObjectId("098765432109876543211234");
+          const postId = new ObjectId("123412345678901234567890");
+          if (id.equals(userId)) {
+              return {
+                  _id: userId,
+                  posts: [postId], // the ids of the Post objects
+                  wardrobe: wardrobeId,
+                  name: "testuser",
+                  followers: 5,
+                  streaks: 10,
+                  bestStreak: 20,
+                  savedPosts: [postId],
+                  achievements: [],
+                  ratedPosts: [{ postId: postId, rating: 3.5 }],
+                  avatarFilename: 'randomfile',
+              };
+          } else {
+              return null;
+          }
+      }),
+  }
+});
