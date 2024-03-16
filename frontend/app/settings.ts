51d21042a982e7d563dce2a0a4332e8bdf1d0a8c
Jason Cheng <jasoncheng224@gmail.com>
2024-03-16 03:01:12 -0700

    Backend host

--- frontend/app/settings.ts
+++ frontend/app/settings.ts
@@ -1,3 +1,3 @@
-export const BACKEND_HOST = (x: any) => x["NEXT_PUBLIC_BACKEND_HOST"]!;
+export const BACKEND_HOST = (x: any) => x["BACKEND_HOST"]!;
 
 export const OAUTH_CLIENT_ID = "121044225700-6gotpenj58iao2fo2qkm573h11c7hbof.apps.googleusercontent.com";
